52321fcc37c97ca011f9fd8f1cb527bee48c21b4
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-02-02 16:40:50 +0900

    Init

---
+++ src/main/kotlin/com/arabyte/arabyteapi/ArabyteApiApplication.kt
@@ -0,0 +1,11 @@
+package com.arabyte.arabyteapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArabyteApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArabyteApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/arabyte/arabyteapi/ArabyteApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.arabyte.arabyteapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArabyteApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
