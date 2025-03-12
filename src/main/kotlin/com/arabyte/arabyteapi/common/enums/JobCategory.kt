820447e516356435202241cd5e8c8d76f747110a
임동준 <127932430+djlim00@users.noreply.github.com>
2025-03-12 21:19:30 +0900

    feat: User 엔티티 생성
    -  개인정보 필드 정의
    - 아르바이트 관심분야 enum클래스 생성

---
+++ src/main/kotlin/com/arabyte/arabyteapi/common/enums/JobCategory.kt
@@ -0,0 +1,5 @@
+package com.arabyte.arabyteapi.common.enums
+
+enum class JobCategory {
+    RESTAURANT, RETAIL, SERVICE, SALES, MANUFACTURING, IT, DESIGN, OFFICE, EDUCATION, DELIVERY, MEDIA, HOSPITAL
+}
