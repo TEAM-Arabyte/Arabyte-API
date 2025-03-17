a579242bb13108e2424802769daadaa22b8ee55b
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-03-17 19:36:46 +0900

    Remove useless code in security configuration

--- src/main/kotlin/com/arabyte/arabyteapi/SecurityConfiguration.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/SecurityConfiguration.kt
@@ -1,33 +1,23 @@
 package com.arabyte.arabyteapi
 
-import com.arabyte.arabyteapi.auth.util.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Profile
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
 
 @EnableWebSecurity
 @Configuration
-class SecurityConfiguration(
-    private val jwtAuthenticationFilter: JwtAuthenticationFilter
-) {
+class SecurityConfiguration {
     @Bean
-    @Profile("local")
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf { it.disable() }
             .authorizeHttpRequests {
-                ////it.requestMatchers("/api/auth/**").permitAll()
-                ////it.anyRequest().authenticated()
                 it.anyRequest().permitAll()
-                //it.requestMatchers("/api/auth/**").permitAll() // 인증 없이 접근 가능
-                //it.anyRequest().authenticated() // 나머지는 인증 필요
             }
             .formLogin { it.disable() }
             .httpBasic { it.disable() }
-        //.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
