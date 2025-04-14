0400891e5e677946faf3691c501237435a760bbd
임동준 <127932430+djlim00@users.noreply.github.com>
2025-04-14 16:15:29 +0900

    Update JwtAuthenticationFilter.kt
    - 필요없는 try-catch문 삭제

--- src/main/kotlin/com/arabyte/arabyteapi/domain/auth/util/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/domain/auth/util/JwtAuthenticationFilter.kt
@@ -1,6 +1,5 @@
 package com.arabyte.arabyteapi.domain.auth.util
 
-import com.arabyte.arabyteapi.global.exception.CustomException
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -26,33 +25,18 @@ class JwtAuthenticationFilter(
             return
         }
 
-
         val token = jwtProvider.resolveToken(request)
+        
+        if (!token.isNullOrBlank() && jwtProvider.isValidToken(token)) {
+            val userId = jwtProvider.getUserId(token)
+            val userDetails: UserDetails =
+                User.withUsername(userId).password("").authorities(emptyList()).build()
+            val authentication = JwtAuthenticationToken(userDetails, token, userDetails.authorities)
+            authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
 
-        try {
-            if (!token.isNullOrBlank() && jwtProvider.isValidToken(token)) {
-                val userId = jwtProvider.getUserId(token)
-                val userDetails: UserDetails =
-                    User.withUsername(userId).password("").authorities(emptyList()).build()
-                val authentication = JwtAuthenticationToken(userDetails, token, userDetails.authorities)
-                authentication.details = WebAuthenticationDetailsSource().buildDetails(request)
-
-                SecurityContextHolder.getContext().authentication = authentication
-            }
-
-            filterChain.doFilter(request, response)
-        } catch (e: CustomException) {
-            response.status = e.customError.status.value()
-            response.contentType = "application/json;charset=UTF-8"
-            val errorJson = """
-            {
-              "code": "${e.customError.name}",
-              "message": "${e.customError.message}"
-            }
-        """.trimIndent()
-            response.writer.write(errorJson)
-            response.writer.flush()
+            SecurityContextHolder.getContext().authentication = authentication
         }
 
+        filterChain.doFilter(request, response)
     }
 }
