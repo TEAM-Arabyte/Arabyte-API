2997e9a97da0bc6e700041e7c34a014f093c1a25
임동준 <127932430+djlim00@users.noreply.github.com>
2025-04-14 16:14:28 +0900

    Update CustomExceptionGroup.kt
    - 필요없는 에러그룹 삭제

--- src/main/kotlin/com/arabyte/arabyteapi/global/enums/CustomExceptionGroup.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/global/enums/CustomExceptionGroup.kt
@@ -12,9 +12,6 @@ enum class CustomExceptionGroup(
             GET_KAKAO_USER_INFO_FAILED
         )
     ),
-    LOGIN_OR_REGISTER(
-        listOf()
-    ),
     REFRESH_ACCESS_TOKEN(
         listOf(
             INVALID_TOKEN,
