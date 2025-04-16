a11cf493c758ad71ba080ff598a9fb2414b3bd85
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-04-17 01:48:21 +0900

    Edit apply to comment

--- src/main/kotlin/com/arabyte/arabyteapi/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/domain/comment/controller/CommentController.kt
@@ -1,7 +1,9 @@
 package com.arabyte.arabyteapi.domain.comment.controller
 
-import com.arabyte.arabyteapi.domain.comment.service.CommentService
 import com.arabyte.arabyteapi.domain.comment.dto.*
+import com.arabyte.arabyteapi.domain.comment.service.CommentService
+import com.arabyte.arabyteapi.domain.user.entity.User
+import com.arabyte.arabyteapi.global.annotation.RequestUser
 import com.arabyte.arabyteapi.global.annotation.SwaggerCustomException
 import com.arabyte.arabyteapi.global.enums.CustomExceptionGroup
 import io.swagger.v3.oas.annotations.Operation
@@ -17,27 +19,30 @@ class CommentController(
     @PostMapping("/{articleId}")
     @SwaggerCustomException(CustomExceptionGroup.COMMENT_CREATE)
     fun createComment(
+        @RequestUser user: User,
         @RequestBody request: CreateCommentRequest
     ): CreateCommentResponse {
-        return commentService.createComment(request)
+        return commentService.createComment(user, request)
     }
 
     @Operation(summary = "댓글 수정", description = "댓글을 수정하는 API입니다.")
     @PutMapping("/{commentId}")
     @SwaggerCustomException(CustomExceptionGroup.COMMENT_UPDATE)
     fun updateArticle(
+        @RequestUser user: User,
         @PathVariable commentId: Long,
         @RequestBody request: UpdateCommentRequest
     ): UpdateCommentResponse {
-        return commentService.updateComment(commentId, request)
+        return commentService.updateComment(user, commentId, request)
     }
 
     @Operation(summary = "댓글 삭제", description = "댓글을 삭제하는 API입니다.")
     @DeleteMapping("/{commentId}")
     @SwaggerCustomException(CustomExceptionGroup.COMMENT_DELETE)
     fun deleteArticle(
+        @RequestUser user: User,
         @PathVariable commentId: Long,
     ): DeleteCommentResponse {
-        return commentService.deleteComment(commentId)
+        return commentService.deleteComment(user, commentId)
     }
 }
--- src/main/kotlin/com/arabyte/arabyteapi/domain/comment/dto/CreateCommentRequest.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/domain/comment/dto/CreateCommentRequest.kt
@@ -2,7 +2,6 @@ package com.arabyte.arabyteapi.domain.comment.dto
 
 data class CreateCommentRequest(
     val articleId: Long,
-    val userId: Long,
     val text: String,
     val parentId: Long?,
     val isAnonymous: Boolean
