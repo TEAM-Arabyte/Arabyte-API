986c0295f4cc993ff96101a12f4f34a4d0f13ced
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-04-17 01:33:40 +0900

    Add request user to auth

--- src/main/kotlin/com/arabyte/arabyteapi/domain/auth/controller/AuthController.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/domain/auth/controller/AuthController.kt
@@ -6,6 +6,8 @@ import com.arabyte.arabyteapi.domain.auth.dto.RegisterResponse
 import com.arabyte.arabyteapi.domain.auth.dto.jwt.ReissueAccessTokenRequest
 import com.arabyte.arabyteapi.domain.auth.dto.jwt.ReissueAccessTokenResponse
 import com.arabyte.arabyteapi.domain.auth.service.AuthService
+import com.arabyte.arabyteapi.domain.user.entity.User
+import com.arabyte.arabyteapi.global.annotation.RequestUser
 import com.arabyte.arabyteapi.global.annotation.SwaggerCustomException
 import com.arabyte.arabyteapi.global.enums.CustomExceptionGroup
 import io.swagger.v3.oas.annotations.Operation
@@ -30,13 +32,13 @@ class AuthController(
         return authService.authorizeWithKakao(kakaoAccessToken)
     }
 
-    // TODO - jwt 처리 필요
     @Operation(summary = "회원가입 정보 수정", description = "회원가입 정보를 업데이트 합니다. 새로운 user객체가 생성되지는 않습니다.")
     @PatchMapping("/register")
     fun register(
+        @RequestUser user: User,
         @RequestBody request: RegisterRequest
     ): RegisterResponse {
-        return authService.registerUser(request)
+        return authService.registerUser(user, request)
     }
 
     @Operation(summary = "액세스 토큰 갱신", description = "refresh token을 이용하여 access token을 갱신합니다.")
