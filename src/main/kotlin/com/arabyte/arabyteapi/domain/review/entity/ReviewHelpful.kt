2e74261e2080fddaeec3047e407ef8792dc19cb9
Ji-InPark <81283634+Ji-InPark@users.noreply.github.com>
2025-05-13 18:29:26 +0900

    Edit review helpful cascade type

--- src/main/kotlin/com/arabyte/arabyteapi/domain/review/entity/ReviewHelpful.kt
+++ src/main/kotlin/com/arabyte/arabyteapi/domain/review/entity/ReviewHelpful.kt
@@ -3,18 +3,15 @@ package com.arabyte.arabyteapi.domain.review.entity
 import com.arabyte.arabyteapi.domain.review.enums.Helpful
 import com.arabyte.arabyteapi.domain.user.entity.User
 import com.arabyte.arabyteapi.global.entity.BaseEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
+import jakarta.persistence.*
 
 @Entity
 class ReviewHelpful(
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     @JoinColumn(name = "review_id")
     val review: Review,
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
     @JoinColumn(name = "user_id")
     val user: User,
 
